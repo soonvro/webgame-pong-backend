d9b64bb4bcd039869ee085b48b19f15f48b98b37
klha <kevinla24@gmail.com>
2024-07-25 15:27:25 +0900

    i did it

--- test_for_communication/backend/index.js
+++ test_for_communication/backend/index.js
@@ -5,12 +5,10 @@ const app = express();
 app.use(cors());
 
 let port_number = 3000;
-let cout = 0;
+let count = 1;
 
-app.get('/api', (req, res) => 
-{
-	res.json({message: `you clicked ${cout++}`});
-	console.log(`req's message is ${req.message}`);
-});
+app.get('/api', (req, res) => {res.json({message: `you clicked ${count++} times`});});
+
+app.get('/', (req, res) => {res.json({message: `you requested basic url`});});
 
 app.listen(port_number, () => {console.log(`backend is running on port ${port_number}`);});
