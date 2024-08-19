4dfd152f77fa46d480d0b45457ccdce02be62e0e
lucku <lsw6731@naver.com>
2024-08-19 18:00:43 +0900

    feat: 유저 api 요청 path 설정

--- Space-Pin-Pong/apps/users/urls.py
+++ Space-Pin-Pong/apps/users/urls.py
@@ -1,10 +1,28 @@
 from django.urls import path
-from .views import UserInfo
+from .views import *
 
 urlpatterns = [
-    path('', UserInfo.as_view()), # GET, DELETE 요청
-    # path('mypage/friends/', ) # GET, POST, DELETE 요청
-    # path('mypage/histories/', ) # GET 요청
-    # path('mypage/nickname/', ) # PUT 요청
-    # path('mypage/picture/', ) # PUT 요청
+    # {42-id}/ GET Authorization: Bearer "access_token"
+    path('<str:user_id>/', UserInfoView.as_view()), # GET, DELETE 요청
+
+    # {42-id}/ DELETE Authorization: Bearer "access_token"
+    path('<str:user_id>/', UserDeleteView.as_view()), # DELETE 요청
+
+    # histories/{42-id} GET Authorization: Bearer "access_token"
+    path('histories/<str:user_id>/', UserHistoryView.as_view()), # GET 요청
+
+    # nicknames/{42-id} PUT Authorization: Bearer "access_token"
+    path('nicknames/<str:user_id>/', UserNicknameUpdateView.as_view()), # PUT 요청
+
+    # pictures/{42-id} PUT Authorization: Bearer "access_token"
+    path('pictures/<str:user_id>/', UserPictureUpdateView.as_view()), # PUT 요청
+
+    # {42-id}/friends/ GET Authorization: Bearer "access_token"
+    path('<str:user_id>/friends/', UserFriendsListView.as_view()), # GET 요청
+
+    # {42-id}/friends/ POST Authorization: Bearer "access_token" {"friend_id": "user"}
+    path('<str:user_id>/friends/', UserFriendAddView.as_view()), # POST 요청
+
+    # {42-id}/friends/{42-friend-id} DELETE Authorization: Bearer "access_token"
+    path('<str:user_id>/friends/<str:friend_id>/', UserFriendDeleteView.as_view()), # DELETE 요청
 ]
--- Space-Pin-Pong/apps/users/views.py
+++ Space-Pin-Pong/apps/users/views.py
@@ -4,5 +4,26 @@ from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
 
-class UserInfo(APIView):
+class UserInfoView(APIView):
+    pass
+
+class UserDeleteView(APIView):
+    pass
+
+class UserHistoryView(APIView):
+    pass
+
+class UserNicknameUpdateView(APIView):
+    pass
+
+class UserPictureUpdateView(APIView):
+    pass
+
+class UserFriendsListView(APIView):
+    pass
+
+class UserFriendAddView(APIView):
+    pass
+
+class UserFriendDeleteView(APIView):
     pass
