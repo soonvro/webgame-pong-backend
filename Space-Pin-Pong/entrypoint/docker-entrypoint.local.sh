63b19ad416c84b35314c57eca6cd7ca1131d6e98
soonvro <soonvro@gmail.com>
2024-08-31 22:53:17 +0900

    env: django 서버 정지 시 자동 시작 기능 추가

--- Space-Pin-Pong/entrypoint/docker-entrypoint.local.sh
+++ Space-Pin-Pong/entrypoint/docker-entrypoint.local.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
 
-echo "Running Space-Pin-Pong Dev Server"
-python manage.py runserver 0.0.0.0:8000
+DJANGO_CMD="python manage.py runserver 0.0.0.0:8000"
+
+echo "+_+| Running Space-Pin-Pong Dev Server"
+$DJANGO_CMD &
+
+while true
+do
+    # 서버 프로세스 확인
+    if ! pgrep -f "$DJANGO_CMD" > /dev/null
+    then
+        echo "+_+| Django 서버가 중지되었습니다. 다시 시작합니다." >> $LOGFILE
+        $DJANGO_CMD &
+    fi
+    sleep 5
+done
