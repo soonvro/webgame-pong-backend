e37f9d314b89ec65fe9be921e2c494aed2d94df3
soonvro <soonvro@gmail.com>
2024-09-02 00:44:12 +0900

    feat: http 프로토콜 라우팅 설정

--- Space-Pin-Pong/config/asgi.py
+++ Space-Pin-Pong/config/asgi.py
@@ -6,7 +6,13 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
-
+from channels.routing import ProtocolTypeRouter
 from django.core.asgi import get_asgi_application
 
-application = get_asgi_application()
+django_asgi_app = get_asgi_application()
+
+application = ProtocolTypeRouter({
+    # Django's ASGI application to handle traditional HTTP requests
+    "http": django_asgi_app,
+})
+
