7b1e35fc889b663de21bfc712873a32fe451abc2
soonvro <soonvro@gmail.com>
2024-09-01 19:19:23 +0900

    refactor: echo 포멧 수정

--- Space-Pin-Pong/entrypoint/docker-entrypoint.sh
+++ Space-Pin-Pong/entrypoint/docker-entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-echo "+_+| Running Database Migrations"
+F_WHITE='\033[38;5;15m'  # foreground white
+B_PURPLE='\033[48;5;200m'  # background purple
+NC='\033[0m' # No Color
+SPEAKER="${F_WHITE}${B_PURPLE}|+_+|${NC}"
+
+/bin/echo -e "${SPEAKER} Running Database Migrations"
 python manage.py makemigrations
 python manage.py migrate
 
