8ed87074201626eb2982cdfe8cd934d9085e44d9
soonvro <soonvro@gmail.com>
2024-09-01 20:25:17 +0900

    env: django 개발 서버를 exec로 실행

--- Space-Pin-Pong/entrypoint/docker-entrypoint.local.sh
+++ Space-Pin-Pong/entrypoint/docker-entrypoint.local.sh
@@ -8,17 +8,4 @@ SPEAKER="${F_WHITE}${B_PURPLE}|+_+|${NC}"
 DJANGO_CMD="python manage.py runserver 0.0.0.0:8000"
 
 /bin/echo -e "${SPEAKER} Running Space-Pin-Pong Dev Server"
-$DJANGO_CMD &
-sleep 5
-
-while true
-do
-    # 서버 프로세스 확인
-    if ! pgrep --full --runstates D,R "$DJANGO_CMD" > /dev/null; then
-        /bin/echo -e "${SPEAKER} Django 서버가 중지되었습니다. 다시 시작합니다."
-        DJANGO_PIDS=$(pgrep -f "$DJANGO_CMD")
-        kill -9 $DJANGO_PIDS
-        $DJANGO_CMD &
-    fi
-    sleep 5
-done
+exec $DJANGO_CMD
