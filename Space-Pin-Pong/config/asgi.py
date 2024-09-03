a9348ed1e269b82eb4437e429b591fe1e2bae1b2
soonvro <soonvro@gmail.com>
2024-09-03 14:09:07 +0900

    feat: websocket 라우팅 추가

--- Space-Pin-Pong/config/asgi.py
+++ Space-Pin-Pong/config/asgi.py
@@ -6,13 +6,17 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+
+from apps.games.routing import websocket_urlpatterns
 
 django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     # Django's ASGI application to handle traditional HTTP requests
     "http": django_asgi_app,
+    # WebSocket handler
+    "websocket": URLRouter(websocket_urlpatterns),
 })
 
