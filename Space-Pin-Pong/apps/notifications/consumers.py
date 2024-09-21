6768593466c9f114940b302c96f6e716ccbd6d35
lucku-1111 <lsw6731@naver.com>
2024-09-22 08:08:24 +0900

    fix: 알림 생성 및 송신 함수 수정(리스트로 데이터 전송)

--- Space-Pin-Pong/apps/notifications/consumers.py
+++ Space-Pin-Pong/apps/notifications/consumers.py
@@ -1,7 +1,5 @@
 from channels.generic.websocket import AsyncWebsocketConsumer
 import json
-from channels.db import database_sync_to_async
-from .models import Notification
 from . import utils
 
 class NotificationConsumer(AsyncWebsocketConsumer):
@@ -24,8 +22,8 @@ class NotificationConsumer(AsyncWebsocketConsumer):
 
     async def send_pending_notifications(self):
         # 처리되지 않은 알림들을 전송
-        notifications = await utils.get_notifications(self.user)
+        notification_list = await utils.get_notifications(self.user)
         self.send(text_data=json.dumps({
             'type': 'notifications',
-            'data': notifications
+            'data': notification_list
         }))
--- Space-Pin-Pong/apps/notifications/utils.py
+++ Space-Pin-Pong/apps/notifications/utils.py
@@ -5,25 +5,24 @@ from .models import Notification
 channel_layer = get_channel_layer()
 
 @database_sync_to_async
-def create_notification(user, message, notification_type):
+def create_and_send_notifications(user, message, notification_type):
     # 알림 데이터베이스에 저장
-    notification = Notification.objects.create(
+    Notification.objects.create(
         user=user,
         type=notification_type,
         message=message
     )
 
+    notification_list = get_notifications(user)
+
     # 웹소켓이 연결되어 있는 경우 실시간 알림 전송
     channel_layer.group_send(
         f'notification_{user.user_id}',
         {
-            'type': notification.type,
-            'id': notification.id,
-            'message': notification.message,
+            'type': 'notifications',
+            'data': notification_list
         }
     )
-
-    return notification
 
 @database_sync_to_async
 def get_notifications(user):
