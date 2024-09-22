9dd0e7a915f936cbee459f83b65168aee3c60e7f
soonvro <soonvro@gmail.com>
2024-09-22 13:57:25 +0900

    feat: RemoteGameInfo model 추가

--- Space-Pin-Pong/apps/games/models.py
+++ Space-Pin-Pong/apps/games/models.py
@@ -1,5 +1,6 @@
 from enum import Enum
 
+from django.conf import settings
 from django.db import models
 
 
@@ -30,3 +31,15 @@ class GameHistory(models.Model):
         db_table = "game_history"
         verbose_name = "게임 기록"
         verbose_name_plural = "게임 기록"
+
+
+class RemoteGameInfo(models.Model):
+    game: models.ForeignKey = models.ForeignKey("GameHistory", on_delete=models.CASCADE)
+    user: models.ForeignKey = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    score: models.IntegerField = models.IntegerField()
+    is_winner: models.BooleanField = models.BooleanField()
+
+    class Meta:
+        db_table = "remote_game_info"
+        verbose_name = "Remote Game Info"
+        verbose_name_plural = "Remote Game Info"
