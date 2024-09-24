863cfd9bf5370d05be82d7737be1ddb1809dfc50
soonvro <soonvro@gmail.com>
2024-09-24 19:37:33 +0900

    feat: 게임 전적 api 추가

--- Space-Pin-Pong/apps/games/views.py
+++ Space-Pin-Pong/apps/games/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from apps.games.serializers import GameStatisticsSerializer
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
-# Create your views here.
+
+@api_view(["GET"])
+@permission_classes([IsAuthenticated])
+def retrieve_game_statistics(request, user_id: str):
+    data: dict = GameStatisticsSerializer.get_statistics(user_id)
+    return Response({"message": "전적 조회를 성공하였습니다.", "data": data})
