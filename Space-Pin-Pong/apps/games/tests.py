a856fedb22bd8a946b6f789a9bb09fd2f12a72e1
soonvro <soonvro@gmail.com>
2024-09-24 19:40:07 +0900

    test: 게임 전적 api 테스트 추가

--- Space-Pin-Pong/apps/games/tests.py
+++ Space-Pin-Pong/apps/games/tests.py
@@ -1,3 +1,66 @@
-from django.test import TestCase
+import json
+from datetime import datetime, timezone
 
-# Create your tests here.
+from apps.games.models import (GameHistory, GameMode, RemoteGameInfo,
+                               Tournament, TournamentGame)
+from apps.games.views import retrieve_game_statistics
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from rest_framework.test import APIRequestFactory, force_authenticate
+
+User = get_user_model()
+
+
+class GameStatisticsTestCase(TestCase):
+    def setUp(self):
+        User.objects.create(user_id="olivia", nickname="nick")
+        User.objects.create(user_id="totoro", nickname="nick")
+        User.objects.create(user_id="mike", nickname="nick")
+        User.objects.create(user_id="jane", nickname="nick")
+        game = GameHistory.objects.create(
+            start_time=datetime(2024, 1, 3, 11, 42, tzinfo=timezone.utc),
+            end_time=datetime(2024, 1, 3, 12, 42, tzinfo=timezone.utc),
+            game_mode=GameMode.REMOTE.value,
+        )
+        RemoteGameInfo.objects.create(game=game, user=User.objects.get(user_id="olivia"), score=100, is_winner=True)
+        RemoteGameInfo.objects.create(game=game, user=User.objects.get(user_id="totoro"), score=10, is_winner=False)
+        tournament = Tournament.objects.create(
+            created_at=datetime(2024, 1, 4, 11, 42, tzinfo=timezone.utc),
+            ended_at=datetime(2024, 1, 4, 12, 42, tzinfo=timezone.utc),
+        )
+        game = GameHistory.objects.create(
+            start_time=datetime(2024, 1, 4, 11, 42, tzinfo=timezone.utc),
+            end_time=datetime(2024, 1, 4, 12, 42, tzinfo=timezone.utc),
+            game_mode=GameMode.TOURNAMENT.value,
+        )
+        TournamentGame.objects.create(game=game, tournament=tournament, round_number=4)
+        RemoteGameInfo.objects.create(game=game, user=User.objects.get(user_id="olivia"), score=100, is_winner=True)
+        RemoteGameInfo.objects.create(game=game, user=User.objects.get(user_id="totoro"), score=10, is_winner=False)
+        game = GameHistory.objects.create(
+            start_time=datetime(2024, 1, 4, 12, 42, tzinfo=timezone.utc),
+            end_time=datetime(2024, 1, 4, 13, 42, tzinfo=timezone.utc),
+            game_mode=GameMode.TOURNAMENT.value,
+        )
+        TournamentGame.objects.create(game=game, tournament=tournament, round_number=4)
+        RemoteGameInfo.objects.create(game=game, user=User.objects.get(user_id="mike"), score=15, is_winner=True)
+        RemoteGameInfo.objects.create(game=game, user=User.objects.get(user_id="jane"), score=1, is_winner=False)
+        game = GameHistory.objects.create(
+            start_time=datetime(2024, 1, 4, 13, 42, tzinfo=timezone.utc),
+            end_time=datetime(2024, 1, 4, 14, 42, tzinfo=timezone.utc),
+            game_mode=GameMode.TOURNAMENT.value,
+        )
+        TournamentGame.objects.create(game=game, tournament=tournament, round_number=2)
+        RemoteGameInfo.objects.create(game=game, user=User.objects.get(user_id="olivia"), score=151, is_winner=True)
+        RemoteGameInfo.objects.create(game=game, user=User.objects.get(user_id="mike"), score=10, is_winner=False)
+
+    def test_retrieve_game_statistics(self):
+        factory = APIRequestFactory()
+        user = User.objects.get(user_id="olivia")
+        view = retrieve_game_statistics
+
+        # Make an authenticated request to the view...
+        request = factory.get("/games/users/olivia")
+        force_authenticate(request, user=user)
+        response = view(request, user_id="olivia")
+        self.assertEqual(response.status_code, 200, response.data)
+        print(json.dumps(response.data, sort_keys=True, indent=2, ensure_ascii=False))
