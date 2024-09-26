302927ca9d6978d49c46a2d92b4e189a9b93b8eb
soonvro <soonvro@gmail.com>
2024-09-26 16:45:43 +0900

    chore: ssl 인증서 추가 및 https,wss 적용

--- Space-Pin-Pong/entrypoint/docker-entrypoint.local.sh
+++ Space-Pin-Pong/entrypoint/docker-entrypoint.local.sh
@@ -5,7 +5,19 @@ B_PURPLE='\033[48;5;200m'  # background purple
 NC='\033[0m' # No Color
 SPEAKER="${F_WHITE}${B_PURPLE}|+_+|${NC}"
 
-DJANGO_CMD="python manage.py runserver 0.0.0.0:8000"
+APP_DOMAIN="api.spp.com"
+
+# Generate SSL certificate
+set -ex \
+    && apt install openssl \
+    && openssl req \
+      -newkey rsa:2048 -nodes -keyout /app/django.key \
+      -x509 -days 365 -out /app/django.crt \
+      -subj "/C=KR/ST=Seoul/L=Seoul/O=42/OU=42 Seoul/CN=${APP_DOMAIN}"
+
+DJANGO_CMD="daphne -b 0.0.0.0 \
+    -e ssl:8443:privateKey=/app/django.key:certKey=/app/django.crt \
+    config.asgi:application"
 
 /bin/echo -e "${SPEAKER} Running Space-Pin-Pong Dev Server"
 exec $DJANGO_CMD
