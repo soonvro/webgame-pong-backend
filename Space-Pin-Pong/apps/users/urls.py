901fdbab1a042dc1adf2e3f8803036e847dfe42f
seoklee <seoklee@student.42seoul.kr>
2024-09-29 13:51:28 +0900

    fix: 들여쓰기 공백으로 변환

--- Space-Pin-Pong/apps/users/urls.py
+++ Space-Pin-Pong/apps/users/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from .views import (FriendAcceptView, FriendRejectView, UserDeactivateView,
                     UserFriendView, UserInfoView, UserUpdateView,
-					UserRecommendView)
+                    UserRecommendView)
 
 urlpatterns = [
     path('nickname/', UserUpdateView.as_view()), # PUT 요청
@@ -10,7 +10,7 @@ urlpatterns = [
     path('friends/', UserFriendView.as_view()), # GET 요청
     path('deactivate/', UserDeactivateView.as_view()), # DELETE 요청
     path('<str:user_id>/info/', UserInfoView.as_view()), # GET, DELETE 요청
-	path('<str:user_id>/recommend/', UserRecommendView.as_view()), # GET 요청
+    path('<str:user_id>/recommend/', UserRecommendView.as_view()), # GET 요청
     path('friends/<str:friend_id>/', UserFriendView.as_view()), # POST, DELETE 요청
     path('friends/<str:friend_id>/accept/', FriendAcceptView.as_view()), # POST 요청
     path('friends/<str:friend_id>/reject/', FriendRejectView.as_view()), # DELETE 요청
